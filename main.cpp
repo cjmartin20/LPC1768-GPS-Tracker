bebcc7a3a392137ac8769cdae0feb97aed12050f
cjmartin20 <cjmartin@eastex.net>
2025-07-11 19:57:04 -0400

    The program is working fine with the Adafruit_GPS package, but still no communication from device. serial connect seems to be issue

--- main.cpp
+++ main.cpp
@@ -1,69 +1,118 @@
 /*
+What's next:
+-understand proper use of Bufferred/Unbuffered serial
+-reintegrate updates to all files
+*/
+
+
+
+/*
 Program to receive data from from GPS module
 
 Written by Colton Martin
 cjmartin@eastex.net
 */
 #include "mbed.h"
-#include "Mbed_Adafruit_GPS.h"
+#include "./Adafruit_GPS/Mbed_Adafruit_GPS.h"
+#include "USBSerial.h"
 #include "stdio.h"
 
 #define GPS_TX_PIN       p9
 #define GPS_RX_PIN       p10
 
 char abuf[1];
-static BufferedSerial ultimateGPS(GPS_TX_PIN, GPS_RX_PIN, 9600);
-static BufferedSerial pc(USBTX, USBRX, 9600); 
-    
-Adafruit_GPS gps(&ultimateGPS);
+static BufferedSerial pc(USBTX, USBRX, 9600);
+static UnbufferedSerial * ultimateGPS; 
+//UnbufferedSerial ultimateGPS(GPS_TX_PIN, GPS_RX_PIN, 9600);
+ultimateGPS(GPS_TX_PIN, GPS_RX_PIN, 9600);
+Adafruit_GPS gpsInstance(&ultimateGPS);
 
 int main() {
-
     DigitalOut led1(LED1);
 
     char c;
     int i,d;
     float latt;
-    gps.begin(9600);
+    bool status;
+    gpsInstance.begin(9600);
+    float testFloat = 1.22;
+    int reset_counter = 0; 
+    
+    char msg[] = "Starting Program...\nTesting Serial Write...\n";
+    printf("Testing printf...\n");
+    pc.write(msg, sizeof(msg));
+    //char command[] = "$PMTK010,002*2D";
+
+    Timer refresh_Timer; //sets up a timer for use in loop; how often do we print GPS info?
+    const int refresh_Time = 2000; //refresh time in ms
+    
+    gpsInstance.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    gpsInstance.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+    gpsInstance.sendCommand(PGCMD_ANTENNA);
+    //gpsInstance.sendCommand(PMTK_Q_RELEASE);
+
     while (true) {
-        //char msg[] = "Testing Serial Write...\n";
-        //pc.write(msg, sizeof(msg));
-        //printf("Testing printf...\n");
-        char sent[] = "Print Works\n";
-        ultimateGPS.read(&sent, sizeof(sent));
-        for (int i = 0; i<3; i++) {
-            pc.write(&sent, sizeof(sent));
-			led1 = 1;
-			ThisThread::sleep_for( 10 );
-			led1 = 0;
-			ThisThread::sleep_for( 45 );
-		}
+        c = gpsInstance.read();
 
-        /*
-        for(int i=0; i<20; i++) {
-            if (gps.newNMEAreceived()){
-                printf("Yes\n");
-            }
-            else
-                printf("No\n");
-            printf("%c", gps.lastNMEA()[i]);
+        if (c) { } //this line will echo the GPS data if not paused
+
+        //check if we recieved a new message from GPS, if so, attempt to parse it,
+        if ( gpsInstance.newNMEAreceived() ) {
+            if ( !gpsInstance.parse(gpsInstance.lastNMEA()) ) {
+                continue;   
+            }    
         }
+
+       //check if enough time has passed to warrant printing GPS info to screen
+       //note if refresh_Time is too low or pc.baud is too low, GPS data may be lost during printing
+        ThisThread::sleep_for( 700ms );
+        printf("Time: %d:%d:%d.%u\n", gpsInstance.hour, gpsInstance.minute, gpsInstance.seconds, gpsInstance.milliseconds);
+        printf("Date: %d/%d/20%d\n", gpsInstance.day, gpsInstance.month, gpsInstance.year);
+        printf("Fix: %i\n", (int) gpsInstance.fix);
+        printf("Quality: %i\n", (int) gpsInstance.fixquality);
+        if (gpsInstance.fix) {
+            printf("Location: %5.2f%c, %5.2f%c\n", gpsInstance.latitude, gpsInstance.lat, gpsInstance.longitude, gpsInstance.lon);
+            printf("Speed: %5.2f knots\n", gpsInstance.speed);
+            printf("Angle: %5.2f\n", gpsInstance.angle);
+            printf("Altitude: %5.2f\n", gpsInstance.altitude);
+            printf("Satellites: %d\n", gpsInstance.satellites);
+        }
+        printf("Update keyword: goose\n\n");
+        /*
+        if (gpsInstance.newNMEAreceived()){
+            printf("Status update received\n");
+            printf("%c", gpsInstance.lastNMEA()[i]);
+        }
+        else
+            printf("No status update\n");
         printf("\n");
-        //if( gps.newNMEAreceived() ) {
-         //   printf("Yes!\n");
-          //  gps.parse( );
-        //}
-        //else {
-         //   print("\tNo Data\n");
-          //  {}
-
-
-        //gps.sendCommand(PMTK_SET_BAUD_9600);
-        //latt = gps.latitude; 
-	    //i = int(latt);
-	    //d = abs(int((latt - float(i)) * 10.0));
-        //printf("Lattitude %i.%i", i, d);
-        ThisThread::sleep_for(100ms);
+        */
+        //ThisThread::sleep_for( 200ms );
+        //gpsInstance.sendCommand(PMTK_SET_NMEA_OUTPUT_ALLDATA);
+        /*
+        if (gpsInstance.waitForSentence("$PMTK314", 50))
+            printf("waitForSentence: true\n");
+        else
+            printf("waitForSentence: false\n");
+        if (gpsInstance.waitForSentence(PMTK_Q_RELEASE))
+            printf("PMTK Awake: true\n");
+        else
+            printf("PMTK Awake: false\n");
+        if (gpsInstance.wakeup())
+            printf("Standby mode was on\n");
+        else
+            printf("Standby mode was off\n");
+        printf("\n");
+        //question - does read or waitForSentence come first
+        if (msg[0] == '0')
+            printf("Read returned \'0\' status\n"); 
+        //printf("String from read: %s\n", msg, sizeof(msg)); 
+        printf("Iteration %i Testing Lattitude: %d\n", reset_counter, gpsInstance.latitude);
+        if(reset_counter > 15){
+            printf("LPC1768 resettings...\n\n\n\n\n");
+            NVIC_SystemReset();
+        }
+        reset_counter += 1; 
         */
     }
 }
